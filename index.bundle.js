9f66708b5c9242c45bbcb7bb0dc9bfdacb619b5a
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2022-11-16 22:13:16 +0530

    setup of eslint and prettier

--- index.bundle.js
+++ index.bundle.js
@@ -1,23 +1,24 @@
-(self["webpackChunkdynamic_ui"] = self["webpackChunkdynamic_ui"] || []).push([["index"],{
+(self.webpackChunkdynamic_ui = self.webpackChunkdynamic_ui || []).push([["index"],{
 
 /***/ "./src/index.js":
-/*!**********************!*\
+/*! **********************!*\
   !*** ./src/index.js ***!
-  \**********************/
+  \********************* */
 /***/ (() => {
 
 function printName(name) {
-    console.log("hi " + name);
+  console.log(`hi ${name}`);
 }
 
-printName('Jack');
+printName("Jack");
+
 
 /***/ })
 
 },
-/******/ __webpack_require__ => { // webpackRuntimeModules
-/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-/******/ var __webpack_exports__ = (__webpack_exec__("./src/index.js"));
-/******/ }
+/** *** */ __webpack_require__ => { // webpackRuntimeModules
+/** *** */ const __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/** *** */ const __webpack_exports__ = (__webpack_exec__("./src/index.js"));
+/** *** */ }
 ]);
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7O0FBQUE7QUFDQTtBQUNBOztBQUVBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vZHluYW1pY191aS8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJmdW5jdGlvbiBwcmludE5hbWUobmFtZSkge1xuICAgIGNvbnNvbGUubG9nKFwiaGkgXCIgKyBuYW1lKTtcbn1cblxucHJpbnROYW1lKCdKYWNrJyk7Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
+// # sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7O0FBQUE7QUFDQSxvQkFBb0IsS0FBSztBQUN6Qjs7QUFFQSIsInNvdXJjZXMiOlsid2VicGFjazovL2R5bmFtaWNfdWkvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiZnVuY3Rpb24gcHJpbnROYW1lKG5hbWUpIHtcbiAgY29uc29sZS5sb2coYGhpICR7bmFtZX1gKTtcbn1cblxucHJpbnROYW1lKFwiSmFja1wiKTtcbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
