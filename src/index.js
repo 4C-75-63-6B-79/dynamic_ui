1166333e5db542f4656e9094f21cc8b0980ba7d2
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2022-11-17 19:33:35 +0530

    Add the dropdown menu to the buttons

--- src/index.js
+++ src/index.js
@@ -20,10 +20,28 @@ function makeButton(buttonName) {
     return button;
 }
 
+function makeDropDownList(list) {
+    const dorpDownList = document.createElement('ul');
+    list.forEach(item => {
+        const listItem = document.createElement('li');
+        listItem.textContent = item;
+        dorpDownList.appendChild(listItem);
+    });
+    return dorpDownList;
+}
+
+function makeDropDownMenu(buttonName, listItems) {
+    const dropDownMenu = document.createElement('div');
+    dropDownMenu.setAttribute('class', 'dropDownMenu');
+    dropDownMenu.appendChild(makeButton(buttonName));
+    dropDownMenu.appendChild(makeDropDownList(listItems));
+    return dropDownMenu;
+}
+
 (function start() {
     const navBar = makeNav();
-    navBar.appendChild(makeButton('Home'));
-    navBar.appendChild(makeButton('Menu'));
-    navBar.appendChild(makeButton('About'));
+    navBar.appendChild(makeDropDownMenu('Home', ['Living Room', 'Bed Room', 'Kitchen']));
+    navBar.appendChild(makeDropDownMenu('Menu', ['TV Menu', 'Restaurant Menu', 'Microwave Menu']));
+    navBar.appendChild(makeDropDownMenu('About', ['About you', 'About someone', 'About anyone']));
 })();
 
