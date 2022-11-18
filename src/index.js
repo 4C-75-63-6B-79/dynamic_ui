532166ca0ad6d004ce62f11d1565adb501e900b5
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2022-11-18 20:17:23 +0530

    will work on small screens

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 import './styles.css';
 
-function printName(name) {
-  console.log(`hi ${name}`);
-}
-
-printName("Jack");
-
 function makeNav() {
     const body = document.querySelector('body');
     const navBar = document.createElement('div');
@@ -37,11 +31,61 @@ function makeDropDownMenu(buttonName, listItems) {
     dropDownMenu.appendChild(makeDropDownList(listItems));
     return dropDownMenu;
 }
+function lineMenuButtonClicked() {
+    const navBar = document.getElementById('navbar');
+    const navDisplayValue = navBar.style.display;
+    const lineMenuButton = document.getElementById('lineMenuButton');
+    if(navDisplayValue === 'none') {
+        lineMenuButton.classList.add('rotate');
+        navBar.style.display = 'flex';
+    } else {
+        lineMenuButton.classList.remove('rotate');
+        navBar.style.display = 'none';
+    }
+}
+
+function makeLineButton() {
+    if(document.getElementById('lineMenuButton')) {
+        return undefined;
+    }
+    const div = document.createElement('div');
+    div.setAttribute('id', 'lineMenuButton');
+    for(let i=0; i<3; i+=1) {
+        const span = document.createElement('span');
+        div.appendChild(span);
+    }
+    div.addEventListener('click', lineMenuButtonClicked, {capture: true});
+    return div;
+}
+
+function removeLineMenuButton(body) {
+    const lineMenuButton = document.getElementById('lineMenuButton');
+    if(lineMenuButton) {
+        body.removeChild(lineMenuButton);
+    }
+}
+
+function windowResized() {
+    const body = document.querySelector('body');
+    const navBar = document.getElementById('navbar');
+    if(window.innerWidth < 400) {
+        navBar.style.display = 'none';
+        const lineButton = makeLineButton();
+        if(lineButton) {
+            body.appendChild(lineButton);
+        }
+    } else {
+        removeLineMenuButton(body);
+        navBar.style.display = 'flex';
+    }
+}
 
 (function start() {
     const navBar = makeNav();
     navBar.appendChild(makeDropDownMenu('Home', ['Living Room', 'Bed Room', 'Kitchen']));
     navBar.appendChild(makeDropDownMenu('Menu', ['TV Menu', 'Restaurant Menu', 'Microwave Menu']));
-    navBar.appendChild(makeDropDownMenu('About', ['About you', 'About someone', 'About anyone']));
+    navBar.appendChild(makeDropDownMenu('About', ['About you', 'About anyone']));
+    windowResized();
 })();
 
+window.addEventListener('resize', windowResized);
